2c9ee6ec4082bcc56698254fa4de438eb4e7d9a4
jdzheng <jzheng98@stanford.edu>
2018-03-07 11:43:58 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# quiz
+# HW2: Personality quiz; Chrome extension 
+
+You are required to complete two parts of the assignment:
+
+- **Personality Quiz**: This is a web page with a Buzzfeed-style personality test. Modify `index.html`, `style.css`, and `script.js` as necessary to complete the web page. 
+  - `constants.js` and `provided-style.css` are provided. You should not need to modify these files; please keep all your JS and CSS in `script.js` and `style.css` respectively. 
+- **Evil Extension**: This is an extension that changes all instances of there/their/they're to the wrong kind of their/there. Modify `evil-extension/page.js` as directed to compete the extension. 
+  - `evil-extension/manifest.json` does not need to be modified.
+
+There is an **optional** part 3 for extra credit:
+- **Twitter Gardener**: When you click "Start Gardening" in the action menu, you can click on Tweets of a Twitter Feed and their text contents change to positive affirmations. This part is totally optional, but if you wish to complete it, you should only need to modify `twitter-gardener-EXTRA-CREDIT/page.js`.
+  - `manifest.json`, `popup.html`, and `popup.js` in the same directory do not have to be modified.
+
+
+See the [Homework 2 spec](http://web.stanford.edu/class/cs193x/homework/2-quiz-ext) for more details and turn-in link.
---
+++ script.js
@@ -0,0 +1,114 @@
+// TODO(you): Write the JavaScript necessary to complete the homework.
+
+// You can access the RESULTS_MAP from "constants.js" in this file since
+// "constants.js" has been included before "script.js" in index.html.
+
+const unchecked_image = "images/unchecked.png"
+const checked_image = "images/checked.png"
+
+let answers =[];
+let finalPicks = new Map();
+
+function changeImage(choice, start, end) {
+  const image = choice.querySelector('.checkbox');
+
+  if(choice.style.backgroundColor !== '#cfe3ff') {
+    if(choice.style.opacity === '0.6') {
+      choice.style.opacity = '1.0';
+    }
+
+    choice.style.backgroundColor = '#cfe3ff';
+    image.src = checked_image;
+    for(let i = start; i<end; i++) {
+      let other = answerChoices[i];
+      if(other!==choice) {
+        other.style.opacity='0.6';
+        other.style.backgroundColor = '#f4f4f4';
+        other.querySelector('.checkbox').src = unchecked_image;
+      }
+    }
+  }
+}
+
+function addAnswers(choice) {
+  if(!answers.includes(choice.dataset.questionId)) {
+    answers.push(choice.dataset.questionId);
+  }
+}
+
+function answerQuestions(choice) {
+  if(choice.dataset.questionId === 'one') {
+    changeImage(choice, 0, 9);
+    addAnswers(choice);
+  } else if(choice.dataset.questionId === 'two') {
+    changeImage(choice, 9, 18);
+    addAnswers(choice);
+  } else {
+    changeImage(choice, 18, 27);
+    addAnswers(choice);
+  }
+}
+
+function selectItem(event) {
+  answerQuestions(event.currentTarget);
+  if(answers.length === 3) {
+    getResults();
+    displayResults();
+  }
+}
+
+function getResults() {
+  for(let i = 0; i<answerChoices.length; i++) {
+    let element = answerChoices[i];
+    if(element.style.backgroundColor === 'rgb(207, 227, 255)') {
+      finalPicks.set(element.dataset.questionId, element.dataset.choiceId);
+    }
+  }
+}
+
+const resultContainer = document.querySelector('#result');
+
+function getText(title, body, key) {
+  title.textContent = "You got:" + ' ' + RESULTS_MAP[key]['title'];
+  body.textContent = RESULTS_MAP[key]['contents'];
+  resultContainer.classList.remove("hidden");
+}
+
+function displayResults() {
+  console.log(finalPicks);
+  const title = document.querySelector('#title');
+  const body = document.querySelector('#body');
+  if(finalPicks.get('two') === finalPicks.get('three')) {
+    getText(title, body, finalPicks.get('two'));
+  } else {
+    getText(title, body, finalPicks.get('one'))
+  }
+
+  for(const option of answerChoices) {
+    option.removeEventListener('click', selectItem);
+  }
+}
+
+function refresh() {
+  resultContainer.classList.add("hidden");
+  for(let i = 0; i<answerChoices.length; i++) {
+    let element = answerChoices[i];
+    const image = element.querySelector('.checkbox');
+    element.addEventListener('click', selectItem);
+    element.style.opacity = '1.0';
+    element.style.backgroundColor = '#f4f4f4';
+    image.src = unchecked_image;
+  }
+
+  answers = [];
+  finalPicks.clear();
+}
+
+let restartQuiz = document.querySelector('#result');
+restartQuiz.addEventListener('click', refresh);
+
+let answerChoices = document.querySelectorAll('.choice-grid div');
+for(let i = 0; i<answerChoices.length; i++) {
+  let element = answerChoices[i];
+	element.addEventListener('click', selectItem);
+}
